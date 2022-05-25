f9e49a1135e79f71356adb335cadd7490c382b8c
Qvisty <jgq@live.dk>
2022-05-25 18:03:44 +0200

    +chapter 6 done

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # DjangoPro40
  My version of Django for Pros 4.0
 
-chapter 5 done
+chapter 6 done
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+# accounts/views.py
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from .forms import CustomUserCreationForm
+
+
+class SignupPageView(generic.CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
+
+
+# Authentication views from https://docs.djangoproject.com/en/4.0/topics/auth/default/#module-django.contrib.auth.views
+# And: https://github.com/django/django/blob/b9cf764be62e77b4777b3a75ec256f6209a57671/django/contrib/auth/urls.py
+# accounts/login/ [name="login"]
+# accounts/logout/ [name="logout"]
+# accounts/password_change/ [name="password_change"]
+# accounts/password_change/done/ [name="password_change_done"]
+# accounts/password_reset/ [name="password_reset"]
+# accounts/password_reset/done/ [name="password_reset_done"]
+# accounts/reset/<uidb64>/<token>/ [name="password_reset_confirm"]
+# accounts/reset/done/ [name="password_reset_complete"]
